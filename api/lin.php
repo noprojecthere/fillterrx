6608ead61857f1c19dbceb161755028ad387b40a
noprojecthere <146555082+noprojecthere@users.noreply.github.com>
2026-02-15 19:18:23 +0530

    Update lin.php

--- api/lin.php
+++ api/lin.php
@@ -1,13 +1,6 @@
 #EXTM3U
 
-#EXTINF:-1 group-title="IND VS PAK" tvg-logo="https://images.slivcdn.com/videoasset_images/manage_file/1000015013/1770729982482912_Best_of_INDvsPAK_10feb_landscape_thumb.jpg?w=1000&q=low",PRIME(HINDI)
-#KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://aqfadtv.xyz/clearkey/results.php?keyid=531oMbLVF1kgD9KaybbnMvLgYKJUTSp4Nb7&key=f5a77d391847790f46059cbb092d575b
-#EXTVLCOPT:http-user-agent=
-#EXTVLCOPT:http-referer=
-#EXTVLCOPT:http-origin=
-#EXTHTTP:{"cookie":""}
-https://ta.bia-cf.live.pv-cdn.net/syd-nitro/live/clients/dash/enc/utvv87801b/out/v1/d9a29796c1c3468785dbee1776ffd354/cenc.mpd
+
 
 #EXTINF:-1 group-title="IND VS PAK" tvg-logo="https://images.slivcdn.com/videoasset_images/manage_file/1000015013/1770729982482912_Best_of_INDvsPAK_10feb_landscape_thumb.jpg?w=1000&q=low",PRIME(ENGLISH)
 #KODIPROP:inputstream.adaptive.license_type=clearkey
@@ -25,12 +18,4 @@ https://ta.bia-cf.live.pv-cdn.net/bom-nitro/live/clients/dash/enc/1pasle3psa/out
 #EXTVLCOPT:http-referer=https://www.hotstar.com/
 #EXTVLCOPT:http-origin=https://www.hotstar.com
 https://livetv-push.hotstar.com/dash/live/2002466-b/sshindiwv/master.mpd|cookie=hdntl=exp=1771213078~acl=*sshindi*~id=01b712bdd9361ce036bbeff76b04c669~data=hdntl~hmac=7a7bdccdb4d19447ea525ea9a8b428a0a5e4e09dcb967e21752b88f4b045f2a7
-
-#EXTINF:-1 group-title="IND VS PAK" tvg-logo="https://images.slivcdn.com/videoasset_images/manage_file/1000015013/1770729982482912_Best_of_INDvsPAK_10feb_landscape_thumb.jpg?w=1000&q=low",STAR ENGLISH
-#KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://aqfadtv.xyz/clearkey/results.php?keyid=fe7718fbb3fb4ba78c07cc0f578744e6&key=624e24b1843b459fab0a949609416f0d
-#EXTVLCOPT:http-user-agent=Hotstar;in.startv.hotstar/SportsLover(Android/15)
-#EXTVLCOPT:http-referer=https://www.hotstar.com/
-#EXTVLCOPT:http-origin=https://www.hotstar.com
-https://livetv-push.hotstar.com/dash/live/2002465/sshd2livetvwv/master.mpd|cookie=hdntl=exp=1771229769~acl=*sshd2livetv*~id=cd7d6024d999aa6782cc38357b329888~data=hdntl~hmac=32aLKnLxfPgRYAafth6JNuU8bHwuPsV1Aaddc3acd305f78b038549a2b86b1d82
 
